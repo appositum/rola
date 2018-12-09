772dd6798c09994f754c0c869a21b5c0a3d33e5a
appositum <appositum@protonmail.com>
2018-12-09 04:16:52 -0300

    Some hspec

--- rola.nix
+++ rola.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, megaparsec_7_0_4, stdenv }:
+{ mkDerivation, base, hspec, megaparsec_7_0_4, stdenv }:
 mkDerivation {
   pname = "rola";
   version = "0.1.0.0";
@@ -7,7 +7,7 @@ mkDerivation {
   isExecutable = true;
   libraryHaskellDepends = [ base megaparsec_7_0_4 ];
   executableHaskellDepends = [ base megaparsec_7_0_4 ];
-  testHaskellDepends = [ base ];
+  testHaskellDepends = [ base hspec megaparsec_7_0_4 ];
   doHaddock = false;
   homepage = "https://github.com/appositum/rola#readme";
   license = stdenv.lib.licenses.asl20;
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nix-shell --run "source ~/.bashrc; cabal v1-run"
+nix-shell --run "source ~/.bashrc; cabal v1-run exe:rola"
