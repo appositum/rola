f3a1875f6dce05fce4e2a1a6543b9e673b562f64
eddie <appositum@protonmail.com>
2018-12-12 18:58:16 -0300

    Safe eval function

--- src/Rola/Eval.hs
+++ src/Rola/Eval.hs
@@ -9,31 +9,41 @@ import Rola.Pretty
 import Rola.Syntax
 import Text.Megaparsec (errorBundlePretty)
 
-envLookup :: Env -> Name -> Expr
+type Error = String
+
+quote :: String -> String
+quote str = "\"" ++ str ++ "\""
+
+envLookup :: Env -> Name -> Either Error Expr
 envLookup env name =
   case M.lookup name env of
-    Just expr -> expr
-    Nothing -> error $ "Couldn't find variable " ++ name
+    Just expr -> Right expr
+    Nothing -> Left $ "Couldn't find variable " ++ quote name
 
-reduce :: Expr -> Expr
+reduce :: Expr -> Either Error Expr
 reduce = reduceEnv M.empty
 
-reduceEnv :: Env -> Expr -> Expr
+reduceEnv :: Env -> Expr -> Either Error Expr
 reduceEnv env (Var var) = envLookup env var
-reduceEnv env (Lam arg body) = Cls arg body env
-reduceEnv env (App func expr) =
-  case reduceEnv env func of
-    Cls arg body closedEnv ->
-      let evaluated = reduceEnv env expr
-          env' = M.insert arg evaluated $ closedEnv `M.union` env
-      in reduceEnv env' body
-    _ -> error "Can't apply this shit"
-reduceEnv _ cls = cls
+reduceEnv env (Lam arg body) = Right $ Cls arg body env
+reduceEnv env (App func expr) = do
+  app <- reduceEnv env func
+  case app of
+    Cls arg body closedEnv -> do
+      evaluated <- reduceEnv env expr
+      let env' = M.insert arg evaluated $ closedEnv `M.union` env
+      reduceEnv env' body
+
+    other -> Left $
+      "Can't apply " ++ quote (prettify other)
+      ++ " to " ++ quote (prettify expr)
+reduceEnv _ cls = Right cls
 
 eval :: String -> IO ()
 eval expr =
   case readExpr expr of
-    Right res -> putStrLn $
-      let reduced = reduce res
-      in prettify reduced ++ "\n -> " ++ show reduced
     Left err -> putStr $ errorBundlePretty err
+    Right res ->
+      case reduce res of
+        Left err -> putStrLn err
+        Right red -> putStrLn $ prettify red ++ "\n -> " ++ show red
